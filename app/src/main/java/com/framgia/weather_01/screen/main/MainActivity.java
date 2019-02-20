cb00257e11129c3b6d4711b91fa17b7db76e96cb
tinngocvo54 <tin.vongoc.54@gmail.com>
2019-02-20 09:55:35 +0700

    Init Api Constaints

--- app/src/main/java/com/framgia/weather_01/screen/main/MainActivity.java
+++ app/src/main/java/com/framgia/weather_01/screen/main/MainActivity.java
@@ -1,7 +1,8 @@
-package com.framgia.weather_01;
+package com.framgia.weather_01.screen.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.framgia.weather_01.R;
 
 public class MainActivity extends AppCompatActivity {
 
